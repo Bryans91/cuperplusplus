7592828ff2f8e3d6b07ff9ec0caf328d51706d65
jertje260 <jwbroekh@avans.nl>
2015-10-30 15:28:49 +0100

    room enums

--- cuperplusplus/cuperplusplus/Room.h
+++ cuperplusplus/cuperplusplus/Room.h
@@ -4,6 +4,31 @@ public:
 	Room();
 	~Room();
 
+	enum Size
+	{
+		SMALL, MEDIUM, LARGE
+	};
+
+	enum State
+	{
+		NEAT, DIRTY
+	};
+
+	enum Furnature
+	{
+		TABLE, BED, EMPTY
+	};
+
+	enum Lighting
+	{
+		CANDLE, TORCH, FIRE
+	};
+
 private:
+	Size size;
+	State state;
+	Furnature furnature;
+	Lighting lighting;
+	std::vector<Room> adjacentRooms;
 
 };
